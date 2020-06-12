19f0fb52646f84072019eb0434c13d5db91934d8
kufre1993 <kufre.esong@yahoo.com>
2020-06-13 04:38:37 +0800

    changed the HTML for the site

---
+++ djangogirls/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shopinverse\desktop\django_girls_toturial\djangogirls\djangogirls\djangogirls\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
